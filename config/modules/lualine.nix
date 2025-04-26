e8f8ba79b0186ab31eeba3f7d8271c728b3230b7
Adan Wodzinski <Adanwodzinski7@gmail.com>
2025-04-26 03:14:45 -0500

    had to revert lualine to earlier code

--- config/modules/lualine.nix
+++ config/modules/lualine.nix
@@ -1,52 +1,14 @@
 {
-  plugins.lualine = {
-    enable = true;
-    settings = {
-      alwaysDivideMiddle = true;
-      globalstatus = true;
-      ignoreFocus = [ "neo-tree" ];
-      extensions = [ "fzf" ];
-      sections = {
-        lualine_a = [ "mode" ];
-        lualine_b = [ "branch" "diff" "diagnostics" ];
-        lualine_c = [ "filename" ];
-        lualine_x = [
-          "encoding"
-          "fileformat"
-          "filetype"
-         {
-            raw = true;
-            lua = "LualineLsp()";
-          }
-        ];
-        lualine_y = [ "progress" ];
-        lualine_z = [ "location" ];
+   plugins.lualine = {
+      enable = true;
+      settings = {
+         alwaysDivideMiddle = true;
+         globalstatus = true;
+         ignoreFocus = [ "neo-tree" ];
+         extensions = [ "fzf" ];
+         options = {
+            theme = "auto";
+         };
       };
-    };
-  };
-
-   extraConfigLua = ''
-      function LualineLsp()
-         local bufnr = vim.api.nvim_get_current_buf()
-         local clients = vim.lsp.get_active_clients()
-
-         if not clients or vim.tbl_isempty(clients) then
-            return ""
-         end
-
-         local lsp_names = {}
-
-         for _, client in ipairs(clients) do
-            if client.attached_buffers and client.attached_buffers[bufnr] then
-               table.insert(lsp_names, client.name)
-            end
-         end
-
-         if #lsp_names == 0 then
-            return ""
-         end
-
-         return " " .. table.concat(lsp_names, " | ")
-      end
-   '';
+   };
 }
