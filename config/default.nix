58be97b60c441bb708b96c2675ef4248af3101fb
Adan Wodzinski <Adanwodzinski7@gmail.com>
2025-04-26 01:08:59 -0500

    created a default.nix for modules

--- config/default.nix
+++ config/default.nix
@@ -5,28 +5,29 @@
       ./options.nix
       ./plugins.nix
 
-      ./modules/comment.nix
-
-      # ./modules/extra_colorschemes.nix
-      # ./modules/colorscheme.nix
-      ./modules/colorscheme
-
-      ./modules/web-devicons.nix
-      ./modules/bufferline.nix
-      ./modules/neotree.nix
-
-      ./modules/transparent.nix
-      
-      ./modules/autopairs.nix
-      ./modules/telescope.nix
-      ./modules/lint.nix
-      ./modules/lsp.nix
-      ./modules/treesitter.nix
-      ./modules/gitsigns.nix
-      ./modules/lualine.nix
-      ./modules/cmp.nix
-
-      ./modules/keybind
+      ./modules
+      # ./modules/comment.nix
+      #
+      # # ./modules/extra_colorschemes.nix
+      # # ./modules/colorscheme.nix
+      # ./modules/colorscheme
+      #
+      # ./modules/web-devicons.nix
+      # ./modules/bufferline.nix
+      # ./modules/neotree.nix
+      #
+      # ./modules/transparent.nix
+      # 
+      # ./modules/autopairs.nix
+      # ./modules/telescope.nix
+      # ./modules/lint.nix
+      # ./modules/lsp.nix
+      # ./modules/treesitter.nix
+      # ./modules/gitsigns.nix
+      # ./modules/lualine.nix
+      # ./modules/cmp.nix
+      #
+      # ./modules/keybind
       # ./modules/keybind/which-key.nix
       # ./modules/keybind/keymap-loader.nix
 
---
+++ config/modules/default.nix
@@ -0,0 +1,26 @@
+{
+   imports = [
+
+      ./comment.nix
+
+      ./colorscheme
+
+      ./web-devicons.nix
+      ./bufferline.nix
+      ./neotree.nix
+
+      ./transparent.nix
+      
+      ./autopairs.nix
+      ./telescope.nix
+      ./lint.nix
+      ./lsp.nix
+      ./treesitter.nix
+      ./gitsigns.nix
+      ./lualine.nix
+      ./cmp.nix
+
+      ./keybind
+
+   ];
+}
