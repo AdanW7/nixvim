16f0837cc5e63ab7ebb5149d9dd6321025f2bde7
Adan Wodzinski <Adanwodzinski7@gmail.com>
2025-04-26 00:39:12 -0500

    updated neotree

--- config/modules/neotree.nix
+++ config/modules/neotree.nix
@@ -1,26 +1,27 @@
 {
-  plugins.neo-tree = {
-    enable = true;
-    enableDiagnostics = true;
-    enableGitStatus = true;
-    enableModifiedMarkers = true;
-    enableRefreshOnWrite = true;
-    closeIfLastWindow = true;
-    popupBorderStyle = "rounded"; # Type: null or one of “NC”, “double”, “none”, “rounded”, “shadow”, “single”, “solid” or raw lua code
-    buffers = {
-      bindToCwd = false;
-      followCurrentFile = {
-        enabled = true;
+   plugins.neo-tree = {
+      enable = true;
+      enableDiagnostics = true;
+      enableGitStatus = true;
+      enableModifiedMarkers = true;
+      enableRefreshOnWrite = true;
+      closeIfLastWindow = true;
+      popupBorderStyle = "rounded"; # Type: null or one of “NC”, “double”, “none”, “rounded”, “shadow”, “single”, “solid” or raw lua code
+      buffers = {
+         bindToCwd = false;
+         followCurrentFile = {
+            enabled = true;
+         };
       };
-    };
-    window = {
-      width = 40;
-      height = 15;
-      autoExpandWidth = false;
-      mappings = {
-        l = "open";
-        h = "close_node";
+      window = {
+         width = 40;
+         height = 15;
+         autoExpandWidth = false;
+         mappings = {
+            l = "open";
+            h = "close_node";
+         };
       };
-    };
-  };
+      addBlankLineAtTop = true;
+   };
 }
