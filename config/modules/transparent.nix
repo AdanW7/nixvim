e0970e28187255e1e37611e52ab433f44e3bd454
Adan Wodzinski <Adanwodzinski7@gmail.com>
2025-04-26 00:56:08 -0500

    added option to transparent.nix to make it work for inactive windows

--- config/modules/transparent.nix
+++ config/modules/transparent.nix
@@ -1,10 +1,10 @@
+# the purpose of this file is to make the main nvim windows clear, when in use and when inactive
 { ... }: {
-  extraConfigLua = ''
-    vim.cmd [[
-      highlight Normal guibg=none
-      highlight NonText guibg=none
-      highlight Normal ctermbg=none
-      highlight NonText ctermbg=none
-    ]]
-  '';
+   extraConfigLua = ''
+      vim.cmd [[
+         highlight Normal ctermbg=none guibg=none
+         highlight NonText ctermbg=none guibg=none
+         highlight NormalNC guibg=none ctermbg=none
+      ]]
+   '';
 }
