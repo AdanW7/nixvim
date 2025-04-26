7bc52d03bad671f1d20b265713b3d55583afca13
Adan Wodzinski <Adanwodzinski7@gmail.com>
2025-04-26 01:31:08 -0500

    moved transparent commands from neotree.nix to transparency.nix

--- config/modules/neotree/neotree.nix
+++ config/modules/neotree/neotree.nix
@@ -25,16 +25,14 @@
             h = "close_node";
          };
       };
-
-      # addBlankLineAtTop = true;
    };
-   extraConfigLua = ''
-      vim.cmd([[
-        highlight! NeoTreeNormal guibg=NONE ctermbg=NONE
-        highlight! NeoTreeNormalNC guibg=NONE ctermbg=NONE
-        highlight! NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
-        highlight! NeoTreeFloatBorder guibg=NONE ctermbg=NONE
-        highlight! NeoTreeFloatTitle guibg=NONE ctermbg=NONE
-      ]])
-    '';
+   # extraConfigLua = ''
+   #    vim.cmd([[
+   #      highlight! NeoTreeNormal guibg=NONE ctermbg=NONE
+   #      highlight! NeoTreeNormalNC guibg=NONE ctermbg=NONE
+   #      highlight! NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
+   #      highlight! NeoTreeFloatBorder guibg=NONE ctermbg=NONE
+   #      highlight! NeoTreeFloatTitle guibg=NONE ctermbg=NONE
+   #    ]])
+   #  '';
 }
--- config/modules/transparent.nix
+++ config/modules/transparent.nix
@@ -7,6 +7,13 @@
          highlight NormalNC guibg=none ctermbg=none
          highlight StatusLine guibg=NONE ctermbg=NONE
          highlight StatusLineNC guibg=NONE ctermbg=NONE
+
+         highlight! NeoTreeNormal guibg=NONE ctermbg=NONE
+         highlight! NeoTreeNormalNC guibg=NONE ctermbg=NONE
+         highlight! NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
+         highlight! NeoTreeFloatBorder guibg=NONE ctermbg=NONE
+         highlight! NeoTreeFloatTitle guibg=NONE ctermbg=NONE
+
       ]]
    '';
 }
